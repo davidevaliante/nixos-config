affdd6bdab2910e433c55b142512a4fc0f48ac6e
davidevaliante <dav.valiante@gmail.com>
2026-05-07 12:19:56 +0200

    changed direnv output noisyness

--- modules/home/programs/direnv.nix
+++ modules/home/programs/direnv.nix
@@ -4,5 +4,8 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config.global.hide_env_diff = true;
   };
+
+  home.sessionVariables.DIRENV_LOG_FORMAT = "\\033[38;5;240mdirenv: %s\\033[0m";
 }
